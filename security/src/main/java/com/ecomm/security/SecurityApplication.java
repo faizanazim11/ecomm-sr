598c0aa9fe908ca504836d4277c84ff7ddb03d62
Faizan Azim <faizanazim11@gmail.com>
2023-04-28 00:14:43 +0530

    enabled logging listeners.

--- security/src/main/java/com/ecomm/security/SecurityApplication.java
+++ security/src/main/java/com/ecomm/security/SecurityApplication.java
@@ -3,11 +3,15 @@ package com.ecomm.security;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.ecomm.security.listeners.LoggingListener;
+
 @SpringBootApplication
 public class SecurityApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SecurityApplication.class, args);
+		SpringApplication app = new SpringApplication(SecurityApplication.class);
+		app.addListeners(new LoggingListener());
+		app.run(args);
 	}
 
 }
